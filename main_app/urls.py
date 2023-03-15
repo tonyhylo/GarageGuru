2345c3fa3fd9bdc7ab5d2441f2ac79fe132f3044
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2023-03-14 20:24:36 -0400

    Added CRUD for posts model.  Added cards on homepage to test CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('messages/', views.messages, name='messages'),
-  path('profile/', views.profile, name='profile'),
+    path('', views.home, name='home'),
+    path('messages/', views.messages, name='messages'),
+    path('profile/', views.profile, name='profile'),
+    path('posts/<int:post_id>/', views.posts_detail, name='detail'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
 ]
