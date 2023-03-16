e56608ce210bbe5e28f86056fd29982f0269b019
Tony Lo <tonylo@Anureets-MacBook-Air.local>
2023-03-15 22:13:22 -0400

    Initialize heroku deployment

--- GarageGuru/settings.py
+++ GarageGuru/settings.py
@@ -130,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
+WSGI_APPLICATION = 'garageguru.wsgi.application'
+# catcollector is the project name
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn garageguru.wsgi
